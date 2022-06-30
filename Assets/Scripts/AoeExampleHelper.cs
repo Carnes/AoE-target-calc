05adc87612b389bfbd7751802ee7291d72515c5a
Carnes <whoismaha@gmail.com>
2022-06-29 23:33:08 -0400

    remove dead code

--- Assets/Scripts/AoeExampleHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace AoeExample
-{
-    public static class AoeExampleHelper
-    {
-        public static float GetRandomAxisLength(float range) => Random.Range(range * -1, range);
-    }
-}
